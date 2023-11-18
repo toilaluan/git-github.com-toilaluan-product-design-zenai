a93122034aa7eb3051bbc9ba8e42b11abf19336c
toilaluan <toilaluan@github.com>
2023-11-18 12:14:55 +0700

    add image report

--- utils.py
+++ utils.py
@@ -5,13 +5,15 @@ from tqdm import tqdm
 from controlnet_aux import CannyDetector, PidiNetDetector
 from controlnet_aux.pidi import PidiNetDetector
 
-pidinet = PidiNetDetector.from_pretrained("lllyasviel/Annotators").to("cuda")
+pidinet = PidiNetDetector.from_pretrained("lllyasviel/Annotators")
 
 # pidi_detector = PidiNetDetector.from_pretrained("lllyasviel/Annotators")
 canny_detector = CannyDetector()
 
+
 def pad64(x):
     return int(np.ceil(float(x) / 64.0) * 64 - x)
+
 
 def HWC3(x):
     assert x.dtype == np.uint8
@@ -30,7 +32,8 @@ def HWC3(x):
         y = color * alpha + 255.0 * (1.0 - alpha)
         y = y.clip(0, 255).astype(np.uint8)
         return y
-    
+
+
 def safer_memory(x):
     return np.ascontiguousarray(x.copy()).copy()
 
@@ -47,12 +50,13 @@ def resize_image_with_pad(input_image, resolution, skip_hwc3=False):
     W_target = int(np.round(float(W_raw) * k))
     img = cv2.resize(img, (W_target, H_target), interpolation=interpolation)
     H_pad, W_pad = pad64(H_target), pad64(W_target)
-    img_padded = np.pad(img, [[0, H_pad], [0, W_pad], [0, 0]], mode='edge')
+    img_padded = np.pad(img, [[0, H_pad], [0, W_pad], [0, 0]], mode="edge")
 
     def remove_pad(x):
         return safer_memory(x[:H_target, :W_target])
 
     return safer_memory(img_padded), remove_pad
+
 
 from typing import List
 from PIL import Image, ImageDraw, ImageOps
@@ -60,12 +64,14 @@ import cv2
 import numpy as np
 import math
 
+
 def prompting(main_object: str):
     prompt = f"{main_object}, textured, high quality, full detailed material, studio style, simple background, dslr, natural lighting, shot by camera, RAW image, photorealistic, sharp focus, 8k, uhd, file grain, masterpiece"
     negative_prompt = "deformed, animation, anime, cartoon, comic, cropped, out of frame, low res, draft, cgi, low quality render, thumbnail"
     return prompt, negative_prompt
 
-def image_grid(images: List[Image.Image], col: int) -> Image.Image:
+
+def image_grid(images: List[Image.Image], col: int, gap: int = 10) -> Image.Image:
     row = math.ceil(len(images) / col)
     # Calculate the width and height of each cell in the grid
     cell_width = images[0].width
@@ -74,7 +80,7 @@ def image_grid(images: List[Image.Image], col: int) -> Image.Image:
     # Create a new image to hold the grid
     grid_width = col * cell_width
     grid_height = row * cell_height
-    grid_image = Image.new('RGB', (grid_width, grid_height))
+    grid_image = Image.new("RGB", (grid_width, grid_height))
 
     # Iterate through the images and paste them into the grid
     for i, image in enumerate(images):
@@ -90,6 +96,48 @@ def image_grid(images: List[Image.Image], col: int) -> Image.Image:
         grid_image.paste(image, (paste_x, paste_y))
 
     return grid_image
+
+
+from PIL import Image, ImageDraw, ImageFont
+import math
+
+
+def image_grid_with_name(
+    images, col_names, row_names, col, row_gap=10, col_gap=10, font_size=20
+):
+    row = math.ceil(len(images) / col)
+    cell_width = images[0].width
+    cell_height = images[0].height
+
+    # Calculate total grid width and height with gaps
+    grid_width = col * cell_width + (col - 1) * col_gap
+    grid_height = row * cell_height + (row - 1) * row_gap
+
+    # Define the size of the text margin for row and column names
+    text_margin = max(font_size, 50)  # Adjust this as needed
+
+    # Create a new image to hold the grid with additional space for text
+    total_width = grid_width + text_margin
+    total_height = grid_height + text_margin
+    grid_image = Image.new("RGB", (total_width, total_height), color="white")
+
+    # Create a drawing context
+    draw = ImageDraw.Draw(grid_image)
+
+    # Iterate through the images and paste them into the grid
+    for i, image in enumerate(images):
+        row_idx = i // col
+        col_idx = i % col
+
+        # Calculate the coordinates to paste the image into the grid
+        paste_x = col_idx * (cell_width + col_gap) + text_margin
+        paste_y = row_idx * (cell_height + row_gap) + text_margin
+
+        # Paste the image into the grid
+        grid_image.paste(image, (paste_x, paste_y))
+
+    return grid_image
+
 
 def resize_square(image: Image.Image, size: int, fill_color=(255, 255, 255)):
     """
@@ -120,7 +168,7 @@ def resize_square(image: Image.Image, size: int, fill_color=(255, 255, 255)):
     resized_image = image.resize((new_width, new_height), Image.BICUBIC)
 
     # Create a new image with the desired size and fill it with the specified color
-    square_image = Image.new('RGB', (size, size), fill_color)
+    square_image = Image.new("RGB", (size, size), fill_color)
 
     # Calculate the position to paste the resized image in the center of the square
     x_offset = (size - new_width) // 2
@@ -131,30 +179,50 @@ def resize_square(image: Image.Image, size: int, fill_color=(255, 255, 255)):
 
     return square_image
 
+
 def preprocess_canny(image, preprocess_image_size, output_image_size):
     image, _ = resize_image_with_pad(np.array(image), preprocess_image_size)
     image = Image.fromarray(image)
-    processed_image = canny_detector(image, detect_resolution=preprocess_image_size, image_resolution=output_image_size)
-    processed_image = resize_square(processed_image, output_image_size, (0,0,0))
+    processed_image = canny_detector(
+        image,
+        detect_resolution=preprocess_image_size,
+        image_resolution=output_image_size,
+    )
+    processed_image = resize_square(processed_image, output_image_size, (0, 0, 0))
     return processed_image
+
 
 def preprocess_sketch(image, preprocess_image_size, output_image_size):
     image, _ = resize_image_with_pad(np.array(image), preprocess_image_size)
     image = Image.fromarray(image)
     processed_image = pidinet(
-       image, detect_resolution=preprocess_image_size, image_resolution=output_image_size, apply_filter=True
+        image,
+        detect_resolution=preprocess_image_size,
+        image_resolution=output_image_size,
+        apply_filter=True,
     )
-    processed_image = resize_square(processed_image, output_image_size, (0,0,0))
+    processed_image = resize_square(processed_image, output_image_size, (0, 0, 0))
     return processed_image
+
 
 def preprocess_sketch_canny(image, preprocess_image_size, output_image_size):
     image, _ = resize_image_with_pad(np.array(image), preprocess_image_size)
     image = Image.fromarray(image)
-    sketch_processed_image = pidi_detector(image, apply_filter=True, detect_resolution=preprocess_image_size, image_resolution=output_image_size)
-    canny_processed_image = canny_detector(image, detect_resolution=preprocess_image_size, image_resolution=output_image_size, low_threshold=150)
+    sketch_processed_image = pidi_detector(
+        image,
+        apply_filter=True,
+        detect_resolution=preprocess_image_size,
+        image_resolution=output_image_size,
+    )
+    canny_processed_image = canny_detector(
+        image,
+        detect_resolution=preprocess_image_size,
+        image_resolution=output_image_size,
+        low_threshold=150,
+    )
     processed_image = np.array(sketch_processed_image) + np.array(canny_processed_image)
-    processed_image = (processed_image > 0)*255
+    processed_image = (processed_image > 0) * 255
     processed_image = np.array(processed_image, dtype=np.uint8)
-    processed_image = Image.fromarray(processed_image) 
-    processed_image = resize_square(processed_image, output_image_size, (0,0,0))
+    processed_image = Image.fromarray(processed_image)
+    processed_image = resize_square(processed_image, output_image_size, (0, 0, 0))
     return processed_image
